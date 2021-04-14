5f6ef158dd34dbfd3477bb49ba9036cea57137ac
rossmfadams <radams9@cub.uca.edu>
2021-04-14 15:38:24 -0500

    Convert to Maven project, add pom.xml, cnn-model, sudoku, sudokusolver

---
+++ sudokuSolver/LinesComparator.java
@@ -0,0 +1,18 @@
+package sudokuSolver;
+import java.util.Comparator;
+import org.deeplearning4j.clustering.cluster.Cluster;
+
+
+/**
+ *
+ * @author Taha Emara
+ * Website: http://www.emaraic.com
+ * Email  : taha@emaraic.com
+ * Created on: Apr 6, 2018 
+ */
+public class LinesComparator implements Comparator<Cluster> {
+	public int compare(Cluster t1, Cluster t2) {
+        return Integer.valueOf((int)t1.getCenter().getArray().getFloat(0)).compareTo(Integer.valueOf((int) t2.getCenter().getArray().getFloat(0)));
+    }
+
+}
