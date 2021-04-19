3e36f056f193ff592f515086c2df66cf6aa7d8b6
shines4 <sandersonhines76@yahoo.com>
2021-04-19 16:54:51 -0500

    first commit didn't work properly, trying again

--- sudokuSolver/ServerCommunication.java
+++ sudokuSolver/ServerCommunication.java
@@ -1,44 +1,80 @@
-package lab2out;
+package sudokuSolver;
+
+import java.awt.Color;
 
 import javax.swing.*;
 
+import lab7out.Database;
 import ocsf.server.AbstractServer;
 import ocsf.server.ConnectionToClient;
 
 public class ServerCommunication extends AbstractServer{
 
 	private JLabel status;
+	private JTextArea log;
+	private Database database = new Database();
+	
 	public ServerCommunication(int port) {
 		super(port);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	protected void handleMessageFromClient(Object arg0, ConnectionToClient arg1) {
-		// TODO Auto-generated method stub
+		 
 		
 	}
 	
-	public void setStatus(JLabel status) {
-		
-	}
+	public void setLog(JTextArea log)
+	  {
+	    this.log = log;
+	  }
+	  
+	  public JTextArea getLog()
+	  {
+	    return log;
+	  }
+	  
+	  public void setStatus(JLabel status)
+	  {
+	    this.status = status;
+	  }
+	  
+	  public JLabel getStatus()
+	  {
+	    return status;
+	  }
 	
-	public void serverStarted() {
-		
-	}
+	protected void serverStarted() 
+	  {
+	    System.out.println("Server Started");
+	    log.append("Server Started\n");
+	  }
+	  
+	  protected void serverStopped() 
+	  {
+	    System.out.println("Server Stopped");
+	    //log.append("Server Stopped Accepting New Clients - Press Listen to Start Accepting New Clients\n");
+	  }
+	  
+	  protected void serverClosed() 
+	  {
+	    System.out.println("Server and all current clients are closed - Press Listen to Restart");
+	    //log.append("Server and all current clients are closed - Press Listen to Restart\n");
+	  }
 
-	public void serverClosed() {
-		
-	}
-	
-	public void clientConnected() {
-		
-	}
-	public void serverStopped() {
-		
-	}
+	  
+	  protected void clientConnected(ConnectionToClient client) 
+	  {
+	    System.out.println("Client Connected");
+	    log.append("Client Connected\n");
+	    status.setText("Connected");
+	    status.setForeground(Color.green);
+	  }
 	
 	protected void listeningException(Throwable exception) {
-		exception.printStackTrace();
+		
+		 System.out.println("Listening Exception:" + exception);
+		    exception.printStackTrace();
+		    System.out.println(exception.getMessage());
 	}
 }
